5f511eb4d84bde8e94939800dfd2e1e40035a78f
ahmadroheed <arkhaliqyar@gmail.com>
2023-04-04 11:14:17 +0430

    Add project files.

---
+++ MethodImplementation/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MethodImplementation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxNumber1 = new System.Windows.Forms.TextBox();
+            this.textBoxNumber2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Call Method";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxNumber1
+            // 
+            this.textBoxNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumber1.Location = new System.Drawing.Point(236, 124);
+            this.textBoxNumber1.Multiline = true;
+            this.textBoxNumber1.Name = "textBoxNumber1";
+            this.textBoxNumber1.Size = new System.Drawing.Size(437, 114);
+            this.textBoxNumber1.TabIndex = 1;
+            // 
+            // textBoxNumber2
+            // 
+            this.textBoxNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumber2.Location = new System.Drawing.Point(236, 257);
+            this.textBoxNumber2.Multiline = true;
+            this.textBoxNumber2.Name = "textBoxNumber2";
+            this.textBoxNumber2.Size = new System.Drawing.Size(437, 114);
+            this.textBoxNumber2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 40);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 546);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNumber2);
+            this.Controls.Add(this.textBoxNumber1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxNumber1;
+        private System.Windows.Forms.TextBox textBoxNumber2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MethodImplementation/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethodImplementation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // access_specifier return_type method_name (parameter_list)
+
+
+        // with out parameter without return type
+
+        public void show()
+        {
+            MessageBox.Show("This is an example of simple method.");
+        }
+
+        public void sumNumber(double a , double b)
+        {
+            double result = a + b;
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //show();
+            try
+            {
+                string n1 = textBoxNumber1.Text;
+                string n2 = textBoxNumber2.Text;
+
+                double x = double.Parse(n1);
+                double y = double.Parse(n2);
+
+
+                sumNumber(x, y);
+            }
+            catch(Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+
+
+        }
+    }
+}
