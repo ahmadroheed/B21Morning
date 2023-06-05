55ea40b87103ecd7987241de04e4835190994ffe
ahmadroheed <arkhaliqyar@gmail.com>
2023-06-05 09:46:27 +0430

    label text color has been changed

--- FirstB21Project/Form1.Designer.cs
+++ FirstB21Project/Form1.Designer.cs
@@ -38,6 +38,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label1.Location = new System.Drawing.Point(206, 113);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(724, 56);
